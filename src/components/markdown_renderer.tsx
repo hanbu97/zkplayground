0924ef19cf1a74e6cb72f8ebcf30a0ff9280f61a
han <hanbu97@proton.me>
2024-09-19 16:13:42 +0800

    tofix: title

--- src/components/markdown_renderer.tsx
+++ src/components/markdown_renderer.tsx
@@ -1,11 +1,12 @@
-// components/MarkdownRenderer.tsx
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkMath from 'remark-math';
 import remarkGfm from 'remark-gfm';
 import rehypeKatex from 'rehype-katex';
 import rehypeRaw from 'rehype-raw';
+import rehypeHighlight from 'rehype-highlight';
 import 'katex/dist/katex.min.css';
+import 'highlight.js/styles/github-dark.css'; // 你可以选择任何你喜欢的高亮样式
 
 type MarkdownRendererProps = {
     content: string;
@@ -15,8 +16,7 @@ const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
     return (
         <ReactMarkdown
             remarkPlugins={[remarkMath, remarkGfm]}
-            rehypePlugins={[rehypeKatex, rehypeRaw]}
-
+            rehypePlugins={[rehypeKatex, rehypeRaw, rehypeHighlight]}
             className="prose prose-invert max-w-none"
         >
             {content}
